409d39e3b0c5c30bcbf2e4aaf7ce80cda0a82c13
Rahmawati Nurul Janah <63674912+rahmawnj@users.noreply.github.com>
2026-09-07 11:47:49 +0700

    feat: add zone master routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\ZoneController;
 use Illuminate\Support\Facades\Route;
 
 Route::inertia('/', 'Welcome')->name('home');
@@ -13,6 +14,11 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('users/{user}', [UserController::class, 'show'])->name('users.show');
     Route::put('users/{user}', [UserController::class, 'update'])->name('users.update');
     Route::delete('users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
+
+    Route::get('zones', [ZoneController::class, 'index'])->name('zones.index');
+    Route::post('zones', [ZoneController::class, 'store'])->name('zones.store');
+    Route::put('zones/{zone}', [ZoneController::class, 'update'])->name('zones.update');
+    Route::delete('zones/{zone}', [ZoneController::class, 'destroy'])->name('zones.destroy');
 });
 
 require __DIR__.'/settings.php';
