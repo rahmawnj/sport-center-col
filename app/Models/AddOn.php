32f0a1548108483f68e07f99d214f76f267d9561
Rahmawati Nurul Janah <63674912+rahmawnj@users.noreply.github.com>
2026-09-07 12:12:58 +0700

    Prepare AddOn model for CRUD

--- app/Models/AddOn.php
+++ app/Models/AddOn.php
@@ -3,8 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class AddOn extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = ['name', 'price', 'stock'];
+
+    protected function casts(): array
+    {
+        return ['price' => 'decimal:2', 'stock' => 'integer'];
+    }
 }
