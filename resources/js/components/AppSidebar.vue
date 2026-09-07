0fa715c71a9b7ebd0fd2d8bb185e877582d277ab
Rahmawati Nurul Janah <63674912+rahmawnj@users.noreply.github.com>
2026-09-07 12:14:08 +0700

    Add remaining master data menus to sidebar

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -1,43 +1,11 @@
 <script setup lang="ts">
 import { Link } from '@inertiajs/vue3';
-import { BookOpen, CircleDot, CircleDollarSign, FolderGit2, LayoutGrid, Users, PanelsTopLeft } from '@lucide/vue';
-import AppLogo from '@/components/AppLogo.vue';
-import NavFooter from '@/components/NavFooter.vue';
-import NavMain from '@/components/NavMain.vue';
-import NavUser from '@/components/NavUser.vue';
-import {
-    Sidebar,
-    SidebarContent,
-    SidebarFooter,
-    SidebarHeader,
-    SidebarMenu,
-    SidebarMenuButton,
-    SidebarMenuItem,
-} from '@/components/ui/sidebar';
-import { dashboard } from '@/routes';
-import type { NavItem } from '@/types';
-
+import { BookOpen, CircleDot, CircleDollarSign, Dumbbell, FolderGit2, LayoutGrid, Users, PanelsTopLeft, Building2, PackagePlus, CreditCard } from '@lucide/vue';
+import AppLogo from '@/components/AppLogo.vue'; import NavFooter from '@/components/NavFooter.vue'; import NavMain from '@/components/NavMain.vue'; import NavUser from '@/components/NavUser.vue';
+import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar'; import { dashboard } from '@/routes'; import type { NavItem } from '@/types';
 const mainNavItems: NavItem[] = [
-    { title: 'Dashboard', href: dashboard(), icon: LayoutGrid },
-    { title: 'Users', href: '/users', icon: Users },
-    { title: 'Zones', href: '/zones', icon: CircleDot },
-    { title: 'Zone Spaces', href: '/zone-spaces', icon: PanelsTopLeft },
-    { title: 'Pricing Rates', href: '/pricing-rates', icon: CircleDollarSign },
+ {title:'Dashboard',href:dashboard(),icon:LayoutGrid},{title:'Users',href:'/users',icon:Users},{title:'Zones',href:'/zones',icon:CircleDot},{title:'Zone Spaces',href:'/zone-spaces',icon:PanelsTopLeft},{title:'Pricing Rates',href:'/pricing-rates',icon:CircleDollarSign},{title:'Membership Packages',href:'/membership-packages',icon:CreditCard},{title:'Trainers',href:'/trainers',icon:Dumbbell},{title:'Facilities',href:'/facilities',icon:Building2},{title:'Add-ons',href:'/add-ons',icon:PackagePlus},
 ];
-
-const footerNavItems: NavItem[] = [
-    { title: 'Repository', href: 'https://github.com/rahmawnj/sport-center-col', icon: FolderGit2 },
-    { title: 'Documentation', href: 'https://laravel.com/docs/starter-kits#vue', icon: BookOpen },
-];
+const footerNavItems: NavItem[]=[{title:'Repository',href:'https://github.com/rahmawnj/sport-center-col',icon:FolderGit2},{title:'Documentation',href:'https://laravel.com/docs/starter-kits#vue',icon:BookOpen}];
 </script>
-
-<template>
-    <Sidebar collapsible="icon" variant="inset">
-        <SidebarHeader>
-            <SidebarMenu><SidebarMenuItem><SidebarMenuButton size="lg" as-child><Link :href="dashboard()"><AppLogo /></Link></SidebarMenuButton></SidebarMenuItem></SidebarMenu>
-        </SidebarHeader>
-        <SidebarContent><NavMain :items="mainNavItems" /></SidebarContent>
-        <SidebarFooter><NavFooter :items="footerNavItems" /><NavUser /></SidebarFooter>
-    </Sidebar>
-    <slot />
-</template>
+<template><Sidebar collapsible="icon" variant="inset"><SidebarHeader><SidebarMenu><SidebarMenuItem><SidebarMenuButton size="lg" as-child><Link :href="dashboard()"><AppLogo /></Link></SidebarMenuButton></SidebarMenuItem></SidebarMenu></SidebarHeader><SidebarContent><NavMain :items="mainNavItems" /></SidebarContent><SidebarFooter><NavFooter :items="footerNavItems" /><NavUser /></SidebarFooter></Sidebar><slot /></template>
