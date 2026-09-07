2178aeee1be05d7f610100b863c991bb32f68dc9
Rahmawati Nurul Janah <63674912+rahmawnj@users.noreply.github.com>
2026-09-07 11:47:23 +0700

    feat: prepare zone master model

--- app/Models/Zone.php
+++ app/Models/Zone.php
@@ -3,8 +3,25 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Zone extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'pricing_model',
+        'is_online_bookable',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'is_online_bookable' => 'boolean',
+        ];
+    }
+
+    public function zoneSpaces(): HasMany
+    {
+        return $this->hasMany(ZoneSpace::class);
+    }
 }
