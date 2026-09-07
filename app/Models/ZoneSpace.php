17306ed1072970e16fb5b44542f6432a2c24695a
Rahmawati Nurul Janah <63674912+rahmawnj@users.noreply.github.com>
2026-09-07 11:53:54 +0700

    Configure ZoneSpace model

--- app/Models/ZoneSpace.php
+++ app/Models/ZoneSpace.php
@@ -3,8 +3,26 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ZoneSpace extends Model
 {
-    //
+    protected $fillable = [
+        'zone_id',
+        'name',
+        'capacity',
+        'status',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'capacity' => 'integer',
+        ];
+    }
+
+    public function zone(): BelongsTo
+    {
+        return $this->belongsTo(Zone::class);
+    }
 }
