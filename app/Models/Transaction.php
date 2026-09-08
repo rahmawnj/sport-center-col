e6e7cd1f836c84b4d02495e9dbb9bdcc24e777cf
Rahmawati Nurul Janah <63674912+rahmawnj@users.noreply.github.com>
2026-09-08 09:16:53 +0700

    Prepare transaction model for public booking

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -3,8 +3,29 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Transaction extends Model
 {
-    //
+    protected $fillable = [
+        'booking_code',
+        'customer_type',
+        'user_id',
+        'guest_name',
+        'payment_method',
+        'total_amount',
+        'payment_status',
+        'booking_status',
+        'handled_by',
+    ];
+
+    public function details(): HasMany
+    {
+        return $this->hasMany(TransactionDetail::class);
+    }
+
+    public function paymentProofs(): HasMany
+    {
+        return $this->hasMany(TransactionPaymentProof::class);
+    }
 }
